9f07a3071307b6a784351f902f03de62473a1cc7
Sorin Chis <kish.sorin@yahoo.com>
2019-10-14 15:48:22 +0300

    added answers using doubly linked list

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,27 +1,29 @@
-import sys
-sys.path.append('../doubly_linked_list')
-#from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList
+# import sys
+# sys.path.append('../doubly_linked_list')
 
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
-        self.storage = []
+        # self.storage = []
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        self.size += 1
-        self.storage.insert(0, value)
+        #self.size += 1
+        # self.storage.insert(0, value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        if self.size:
-            front_el = self.storage[-1]
-            del self.storage[-1]
-            self.size -= 1
-            return front_el
+        if self.storage.length:
+            #front_el = self.storage[-1]
+            #del self.storage[-1]
+            #self.size -= 1
+            # return front_el
+            return self.storage.remove_from_head()
         else:
             return
 
     def len(self):
-        return self.size
+        return self.storage.length
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,27 +1,29 @@
-#from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
 
 
 class Stack:
     def __init__(self):
-        self.size = 0
+        #self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
-        self.storage = []
+        #self.storage = []
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        self.size += 1
-        self.storage.append(value)
+        #self.size += 1
+        # self.storage.append(value)
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        if self.size:
-            pop_element = self.storage[-1]
-            self.storage.pop()
-            self.size -= 1
-            return pop_element
+        if self.storage.length:
+            #pop_element = self.storage[-1]
+            # self.storage.pop()
+            #self.size -= 1
+            return self.storage.remove_from_tail()
         else:
             return
 
     def len(self):
-        return self.size
+        return self.storage.length
