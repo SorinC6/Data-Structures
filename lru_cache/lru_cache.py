f13cff4b67403edab99ea3ba963ef638d5cdd592
Sorin Chis <kish.sorin@yahoo.com>
2019-10-16 10:49:34 +0300

    divide the comments to be more  clear

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -34,11 +34,15 @@ class LRUCache:
         return value
 
     """
-    Adds the given key-value pair to the cache. The newly-
+    Case 1: Adds the given key-value pair to the cache. The newly-
     added pair should be considered the most-recently used
-    entry in the cache. If the cache is already at max capacity
+    entry in the cache.
+​
+    Case 2: If the cache is already at max capacity
     before this entry is added, then the oldest entry in the
-    cache needs to be removed to make room. Additionally, in the
+    cache needs to be removed to make room.
+    ​
+    Case 3: Additionally, in the
     case that the key already exists in the cache, we simply
     want to overwrite the old value associated with the key with
     the newly-specified value.
@@ -46,20 +50,20 @@ class LRUCache:
 
     def set(self, key, value):
 
-        # 
+        # 3
         if key in self.cache:
             node = self.cache[key]
             self.order.move_to_front(node)
             node.value = (key, value)
 
         else:
-            # 
+            # 2
             if self.current_nodes == self.max_number_of_nodes:
                 del self.cache[self.order.tail.value[0]]
                 self.order.remove_from_tail()
                 self.current_nodes -= 1
 
-            # 
+            # 1
             self.order.add_to_head((key, value))
             self.cache[key] = self.order.head
             self.current_nodes += 1
